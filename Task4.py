0402a158bf439ac2ab0d07858fba6e78f50c47d9
Tatiana <Tatiana_novik@mail.ru>
2022-07-30 17:11:09 +0700

    решена четвертая задача

--- Task4.py
+++ Task4.py
@@ -1,3 +1,32 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+
+
+count = 1
+with open('file_4_1.txt', 'r') as data:
+    my_data = data.read()
+    print(my_data)
+
+prev = 'first'
+coef = str
+res = ''
+
+for item in my_data:
+    if prev == 'first':
+        prev = item
+    else:
+        if item == prev:
+            count += 1
+        else:
+            coef = str(count)
+            res += coef + prev
+            prev = item
+            count = 1
+print(res)
+
+data_1 = open('file_4_2.txt', 'a')
+data_1.write(res)
+data_1.close()
+
